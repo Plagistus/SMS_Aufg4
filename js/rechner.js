3da8cbcfe88f28dcea706f4369148bb14c2b5690
caelumn <40457307@live.napier.ac.uk>
2021-04-15 18:33:03 +0200

    Ajax umrechnung

--- js/rechner.js
+++ js/rechner.js
@@ -2,14 +2,25 @@
 
 function Model() {
     var self = this,
-        rate = 0.91;   //Date: 10.04.2021
+        rate;   //Date: 10.04.2021
     let chf;
-
     self.observable = new Observable();
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "text.txt", true);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            this.responseText;
+            rate = xhttp.responseText;
+            console.log("Rate: "+rate);
+            getEUR()
+        }
+    };
+    xhttp.send();
 
     function getEUR() {
         var euro = chf * rate;
-
+        console.log("getEUR:"+ rate);
         //Ausgabe Euro nur mit zwei Nachkommastellen
         euro = euro.toFixed(2)
         self.observable.notifyObservers(euro);
